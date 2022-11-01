b197ddfc281d2533945413cbf31403d98f837f0f
WallSid <103652593+WallSid@users.noreply.github.com>
2022-11-01 20:44:06 +0200

    lang switcher rdy

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,25 @@
+// const langEn = document.querySelector('#lang-en');
+// const langRu = document.querySelector('#lang-ru');
+
+// langEn.addEventListener('click', setLang.bind(null, 'en'));
+// langRu.addEventListener('click', setLang.bind(null, 'ru'));
+
+// function setLang(lang) {
+//   if (!langArr.hasOwnProperty(lang)) return;
+//   if (window.hasOwnProperty('localStorage'))
+//     window.localStorage.setItem('lang', lang);
+//   for (let key in langArr[lang]) {
+//     let elem = document.querySelector('.lng-' + key);
+//     if (elem) {
+//       elem.innerHTML = langArr[lang][key];
+//     }
+//   }
+//   location.reload();
+// }
+// let lang = window.hasOwnProperty('localStorage') || 'en';
+// setLang(lang);
+
+
 const select = document.querySelector('select');
 const allLang = ['en', 'ua'];
 
@@ -22,8 +44,8 @@ function changeLanguage () {
     // elem = document.querySelector('[data-att]');
     // console.log(elem);
     console.log(document.querySelector('.lng-title').innerHTML);
-    for (let key in langArray) {
-        document.querySelector('.lng-' + key).innerHTML = langArray[key][hash];
+    for (let key in langArr) {
+        document.querySelector('.lng-' + key).innerHTML = langArr[key][hash];
     };
 
 }
--- js/lang.js
+++ js/lang.js
@@ -1,14 +1,19 @@
-const langArray = {
-    "unit" : {
-        "ua": "Онлайн ресторан",
-        "en": "Online restraunt",
-    },
+const langArr = {
     "title" : {
-        "ua": "Онлайн ресторан",
-        "en": "Online restraunt",
+        "ua": `Онлайн ресторан <span class="accent__logo">GOOD</span>FOOD`,
+        "en": `Online restraunt <span class="accent__logo">GOOD</span>FOOD`,
     },
     "suptitle" : {
         "ua": "Замовляй здорову їжу з доставкою у будь-який район Києва в новому мобільному додатку",
         "en": "Order healthy food with delivery to any district of Kyiv in the new mobile application",
     },
+    // "en": {
+    //     "title": "Online restraunt",
+    //     "suptitle": "Order healthy food with delivery to any district of Kyiv in the new mobile application",
+    // },
+    // "ru": {
+    //     "title": "Онлайн ресторан",
+    //     "suptitle": "Замовляй здорову їжу з доставкою у будь-який район Києва в новому мобільному додатку",
+    // },
+
 }
